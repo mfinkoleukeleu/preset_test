3db73396a7d57174d4547fd8389d8cb5fa8a5045
Maryna Finko <129845613+mfinkoleukeleu@users.noreply.github.com>
2024-07-17 16:35:54 +0200

    Update index.js

--- inputtext/index.js
+++ inputtext/index.js
@@ -1,61 +1,54 @@
 export default {
-  root: ({ props, context, parent }) => ({
-    class: [
-      // Font
-      'font-sans leading-none',
+    root: ({ props, context, parent }) => ({
+        class: [
+            // Font
+            'leading-[normal]',
 
-      // Flex
-      { 'flex-1 w-[1%]': parent.instance.$name == 'InputGroup' },
+            // Flex
+            { 'flex-1 w-[1%]': parent.instance.$name == 'InputGroup' },
 
-      // Size & Spacing
-      'h-9',
-      'm-0',
-      {
-        'px-4 py-4': props.size == 'large',
-        'px-2 py-2': props.size == 'small',
-        'p-3': props.size == null,
-      },
+            // Spacing
+            'm-0',
+            { 'w-full': props.fluid },
 
-      // Shape
-      { rounded: parent.instance.$name !== 'InputGroup' },
-      {
-        'first:rounded-l-md rounded-none last:rounded-r-md':
-          parent.instance.$name == 'InputGroup',
-      },
-      {
-        'border-0 border-y border-l last:border-r':
-          parent.instance.$name == 'InputGroup',
-      },
-      {
-        'first:ml-0 -ml-px':
-          parent.instance.$name == 'InputGroup' && !props.showButtons,
-      },
+            // Size
+            {
+                'px-4 py-4': props.size == 'large',
+                'px-2 py-2': props.size == 'small',
+                'p-3': props.size == null
+            },
 
-      // Colors
-      'text-surface-600',
-      'placeholder:text-surface-400',
-      'bg-surface-0',
-      'border',
-      { 'border-surface-300': !props.invalid },
+            // Shape
+            { 'rounded-md': parent.instance.$name !== 'InputGroup' },
+            { 'first:rounded-l-md rounded-none last:rounded-r-md': parent.instance.$name == 'InputGroup' },
+            { 'border-0 border-y border-l last:border-r': parent.instance.$name == 'InputGroup' },
+            { 'first:ml-0 -ml-px': parent.instance.$name == 'InputGroup' && !props.showButtons },
 
-      // Invalid State
-      { 'border-red-500': props.invalid },
+            // Colors
+            'text-surface-800 dark:text-white/80',
+            'placeholder:text-surface-400 dark:placeholder:text-surface-500',
+            'bg-surface-0 dark:bg-surface-900',
+            'border',
+            { 'border-surface-300 dark:border-surface-600': !props.invalid },
 
-      // States
-      {
-        'hover:border-primary-500': !context.disabled && !props.invalid,
-        'focus:outline-none focus:ring focus:ring-primary-400/50 focus:z-10':
-          !context.disabled,
-        'bg-surface-200 text-surface-500 opacity-60 select-none pointer-events-none cursor-default':
-          context.disabled,
-      },
+            // Invalid State
+            'invalid:focus:ring-red-200',
+            'invalid:hover:border-red-500',
+            { 'border-red-500 dark:border-red-400': props.invalid },
 
-      // Filled State *for FloatLabel
-      { filled: parent.instance?.$name == 'FloatLabel' && context.filled },
+            // States
+            {
+                'hover:border-primary': !context.disabled && !props.invalid,
+                'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50 focus:z-10': !context.disabled,
+                'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+            },
 
-      // Misc
-      'appearance-none',
-      'transition-colors duration-200',
-    ],
-  }),
-}
+            // Filled State *for FloatLabel
+            { filled: parent.instance?.$name == 'FloatLabel' && context.filled },
+
+            // Misc
+            'appearance-none',
+            'transition-colors duration-200'
+        ]
+    })
+};
